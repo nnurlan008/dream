0f4149180f73afa32292ea2591b316c42419ae8e
Nurlan Nazaraliyev <nnaza008@ucr.edu>
2024-02-12 09:08:27 -0800

    initial commit: post poll for gpu

---
+++ gpu-utils.h
@@ -0,0 +1,66 @@
+#ifndef GPU_UTILS_H
+#define GPU_UTILS_H
+
+#include <infiniband/verbs.h>
+
+static const int RDMA_BUFFER_SIZE = 1024;
+
+struct message {
+  enum {
+    MSG_MR,
+    MSG_DONE
+  } type;
+
+  union {
+    struct ibv_mr mr;
+  } data;
+};
+
+struct context {
+  struct ibv_context *ctx;
+  struct ibv_pd *pd;
+  struct ibv_cq *cq;
+  struct ibv_comp_channel *comp_channel;
+
+  pthread_t cq_poller_thread;
+};
+
+struct connection {
+  struct rdma_cm_id *id;
+  struct ibv_qp *qp;
+
+  int connected;
+
+  struct ibv_mr *recv_mr;
+  struct ibv_mr *send_mr;
+  struct ibv_mr *rdma_local_mr;
+  struct ibv_mr *rdma_remote_mr;
+
+  struct ibv_mr peer_mr;
+
+  struct message *recv_msg;
+  struct message *send_msg;
+
+  char *rdma_local_region;
+  char *rdma_remote_region;
+
+  enum {
+    SS_INIT,
+    SS_MR_SENT,
+    SS_RDMA_SENT,
+    SS_DONE_SENT
+  } send_state;
+
+  enum {
+    RS_INIT,
+    RS_MR_RECV,
+    RS_DONE_RECV
+  } recv_state;
+};
+
+enum mode {
+  M_WRITE,
+  M_READ
+};
+
+#endif
